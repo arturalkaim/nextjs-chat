2fe0f271e348277b1121ad883971d41d442473ba
Artur Alkaim <artur@nezasa.com>
2024-04-24 15:07:20 +0100

    update examples

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -36,24 +36,14 @@ export function ChatPanel({
 
   const exampleMessages = [
     {
-      heading: 'What are the',
-      subheading: 'trending memecoins today?',
-      message: `What are the trending memecoins today?`
-    },
-    {
-      heading: 'What is the price of',
-      subheading: '$DOGE right now?',
-      message: 'What is the price of $DOGE right now?'
-    },
-    {
-      heading: 'I would like to buy',
-      subheading: '42 $DOGE',
-      message: `I would like to buy 42 $DOGE`
+      heading: 'Plan my trip to Portugal',
+      subheading: 'for 4 days in Lisbon and 3 days in Porto',
+      message: 'Plan my trip to Portugal for 4 days in Lisbon and 3 days in Porto?'
     },
     {
       heading: 'What are some',
-      subheading: `recent events about $DOGE?`,
-      message: `What are some recent events about $DOGE?`
+      subheading: `interesting cities to visit in Spain?`,
+      message: `What are some interesting cities to visit in Spain?`
     }
   ]
 
